1af7f13cf81ff4bf525c871900ff0d6c30bb1482
hangrybear666 <hangrybear666+github@proton.me>
2025-10-20 06:10:37 +0000

    Adds production grade nginx & Docker hosting setup

---
+++ src/vite-env.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="vite/client" />
+
+// defined in vite.config.ts and dynamically loaded based on ENV VAR passed in pipeline for builds
+declare const __APP_VERSION__: string;
--- vite.config.ts
+++ vite.config.ts
@@ -10,11 +10,22 @@ const FRONTEND_PORT = Number(process.env.FRONTEND_PORT) || 3003;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __APP_VERSION__: JSON.stringify(
+      process.env.VITE_BUILD_VERSION
+        ? `${process.env.VITE_BUILD_VERSION}`
+        : 'local-development'
+    ),
+  },
   server: {
     watch: {
       usePolling: true // Add this line if you're running on a network filesystem or Docker and Hot Module Replace is not working
     },
     host: true,
     port: FRONTEND_PORT
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
   }
 });
